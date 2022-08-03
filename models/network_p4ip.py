61546ecd19e30ccee0850e08e0ae954697392bed
Luke Li <lta19@mails.tsinghua.edu.cn>
2022-08-03 20:57:15 +0800

    Update network_p4ip.py

--- models/network_p4ip.py
+++ models/network_p4ip.py
@@ -57,43 +57,7 @@ class Down(nn.Module):
 	def forward(self, x):
 		return self.maxpool_conv(x)
 
-class X_Update(nn.Module):
-	def __init__(self):
-		super(X_Update, self).__init__()
 
-	def forward(self, x1, x2, AtA_fft, rho1, rho2):
-		lhs = rho1*AtA_fft + rho2 
-		rhs = tfft.fftn( rho1*x1 + rho2*x2, dim=[2,3] )
-		x = tfft.ifftn(rhs/lhs, dim=[2,3])
-		return x.real
-
-class V_Update(nn.Module):
-	def __init__(self):
-		super(V_Update, self).__init__()
-
-	def forward(self, v_tilde, y, rho2, M):
-		t1 = rho2*v_tilde - M 
-		return 0.5*(1/rho2)*(-t1 + torch.sqrt(t1**2 + 4*y*rho2))
-
-class Z_Update(nn.Module):
-	"""Updating Z with zero partial derivative."""
-	def __init__(self):
-		super(Z_Update, self).__init__()		
-
-	def forward(self, z):
-		z_out = self.net(z.float())
-		return z_out
-
-class Z_Update_ResUNet(nn.Module):
-	"""Updating Z with ResUNet as denoiser."""
-	def __init__(self):
-		super(Z_Update_ResUNet, self).__init__()		
-		self.net = ResUNet()
-
-	def forward(self, z):
-		z_out = self.net(z.float())
-		return z_out
-	
 class InitNet(nn.Module):
 	def __init__(self, n):
 		super(InitNet,self).__init__()
@@ -110,55 +74,98 @@ class InitNet(nn.Module):
 			nn.ReLU(inplace=True),
 			nn.Linear(64, 64),
 			nn.ReLU(inplace=True),
-			nn.Linear(64, 2*self.n),
+			nn.Linear(64, 3*self.n),
 			nn.Softplus())
 		self.resize = nn.Upsample(size=[256,256], mode='bilinear', align_corners=True)
 		
 	def forward(self, kernel, M):
-		N, C, H, W  = kernel.size()
-		h1, h2 = int(np.floor(0.5*(128-H))), int(np.ceil(0.5*(128-H)))
-		w1, w2 = int(np.floor(0.5*(128-W))), int(np.ceil(0.5*(128-W)))
+		N, _, h, w  = kernel.size()
+		h1, h2 = int(np.floor(0.5*(128-h))), int(np.ceil(0.5*(128-h)))
+		w1, w2 = int(np.floor(0.5*(128-w))), int(np.ceil(0.5*(128-w)))
 		k_pad = F.pad(kernel, (w1,w2,h1,h2), "constant", 0)
-		A = tfft.fftn(k_pad,dim=[2,3])
-		AtA_fft = torch.abs(A)**2
-		x = self.conv_layers(AtA_fft.float())
+		H = tfft.fftn(k_pad,dim=[2,3])
+		HtH_fft = torch.abs(H)**2
+		x = self.conv_layers(HtH_fft.float())
 		x = torch.cat((x.view(N,1,16*8*8),  M.float().view(N,1,1)), axis=2).float()
-		h = self.mlp(x)+1e-6
+		output = self.mlp(x)+1e-6
 
-		rho1_iters = h[:,:,0:self.n].view(N, 1, 1, self.n)
-		rho2_iters = h[:,:,self.n:2*self.n].view(N, 1, 1, self.n)
-		return rho1_iters, rho2_iters
-		
-		
+		rho1_iters = output[:,:,0:self.n].view(N, 1, 1, self.n)
+		rho2_iters = output[:,:,self.n:2*self.n].view(N, 1, 1, self.n)
+		lam_iters = output[:,:,2*self.n:3*self.n].view(N, 1, 1, self.n)
+		return rho1_iters, rho2_iters, lam_iters
+
+
+class X_Update(nn.Module):
+	def __init__(self):
+		super(X_Update, self).__init__()
+
+	def forward(self, x0, x1, HtH_fft, rho1, rho2):
+		lhs = rho1*HtH_fft + rho2 
+		rhs = tfft.fftn(rho1*x0 + rho2*x1, dim=[2,3] )
+		x = tfft.ifftn(rhs/lhs, dim=[2,3])
+		return x.real
+
+class V_Update(nn.Module):
+	def __init__(self):
+		super(V_Update, self).__init__()
+
+	def forward(self, v_tilde, y, rho2, alpha):
+		t1 = rho2*v_tilde - alpha 
+		return 0.5*(1/rho2)*(-t1 + torch.sqrt(t1**2 + 4*y*rho2))
+
+class Z_Update(nn.Module):
+	"""Updating Z with l1 norm."""
+	def __init__(self):
+		super(Z_Update, self).__init__()		
+
+	def forward(self, z_tilde, lam, rho1):
+		z_out = torch.sign(z_tilde) * torch.max(0, torch.abs(z_tilde) - lam/rho1)
+		return z_out
+
+class Z_Update_ResUNet(nn.Module):
+	"""Updating Z with ResUNet as denoiser."""
+	def __init__(self):
+		super(Z_Update_ResUNet, self).__init__()		
+		self.net = ResUNet()
+
+	def forward(self, z):
+		z_out = self.net(z.float())
+		return z_out
+
+
 class P4IP_Net(nn.Module):
-	def __init__(self, n_iters=8):
+	def __init__(self, n_iters=8, PnP=True):
 		super(P4IP_Net, self).__init__()
 		self.n =  n_iters
+		self.pnp = PnP
 		self.init = InitNet(self.n)
 		self.X = X_Update() # FFT based quadratic solution
 		self.V = V_Update()	# Poisson MLE
-		self.Z = Z_Update_ResUNet() # BW Denoiser
+		if self.pnp:
+			self.Z = Z_Update_ResUNet() # BW Denoiser
+		else:
+			self.Z = Z_Update() # denoise with l1 norm
 	
-	def init_l2(self, y, A, M):
-		N, C, H, W = y.size()
-		At, AtA_fft = torch.conj(A), torch.abs(A)**2
-		rhs = tfft.fftn( conv_fft_batch(At, y/M), dim=[2,3] )
-		lhs = AtA_fft + (1/M)
+	def init_l2(self, y, H, M):
+		# N, C, H, W = y.size()
+		Ht, HtH_fft = torch.conj(Ht), torch.abs(H)**2
+		rhs = tfft.fftn( conv_fft_batch(Ht, y/M), dim=[2,3] )
+		lhs = HtH_fft + (1/M)
 		x0 = torch.real(tfft.ifftn(rhs/lhs, dim=[2,3]))
 		x0 = torch.clamp(x0,0,1)
 		return x0
 
-	def forward(self, y, kernel, M):
+	def forward(self, y, kernel, alpha):
 		device = torch.device("cuda:0" if y.is_cuda else "cpu")
 		x_list = []
 
-		N, C, H, W = y.size()
+		N, _, _, _ = y.size()
 		# Generate auxiliary variables for convolution
-		k_pad, A = psf_to_otf(kernel, y.size())
-		A =  A.to(device)
-		At, AtA_fft = torch.conj(A), torch.abs(A)**2
-		rho1_iters, rho2_iters = self.init(kernel, M) 	# Hyperparameters
-		x = self.init_l2(y, A, M)	# Initialization using Weiner Deconvolution
+		k_pad, H = psf_to_otf(kernel, y.size())
+		H =  H.to(device)
+		At, HtH_fft = torch.conj(H), torch.abs(H)**2
+		rho1_iters, rho2_iters, lam_iters = self.init(kernel, alpha) 	# Hyperparameters
+		x = self.init_l2(y, H, alpha)	# Initialization using Weiner Deconvolution
 		x_list.append(x)
 		# Other ADMM variables
 		z = Variable(x.data.clone()).to(device)
@@ -169,15 +176,17 @@ class P4IP_Net(nn.Module):
 		for n in range(self.n):
 			rho1 = rho1_iters[:,:,:,n].view(N,1,1,1)
 			rho2 = rho2_iters[:,:,:,n].view(N,1,1,1)
-			# U, Z and X updates
-			v = self.V(conv_fft_batch(A,x) + u1, y, rho1, M)
-			z = self.Z(x + u2)
-			x = self.X(conv_fft_batch(At,v - u1), z - u2, AtA_fft, rho1, rho2)
-			# Lagrangian updates			
-			u1 = u1 + conv_fft_batch(A,x) - v
-			u2 = u2 + x - z
+			lam = lam_iters[:,:,:,n].view(N,1,1,1)
+			# V, Z and X updates
+			v = self.V(conv_fft_batch(H,x) + u2, y, rho2, alpha)
+			if self.pnp:
+				z = self.Z(x + u1)
+			else:
+				z = self.Z(x + u1, lam, rho1)
+			x = self.X(z - u1, conv_fft_batch(At,v - u2), HtH_fft, rho1, rho2)
+			# Lagrangian updates
+			u1 = u1 + x - z			
+			u2 = u2 + conv_fft_batch(H,x) - v
 			x_list.append(x)
 
 		return x_list
-
-	
